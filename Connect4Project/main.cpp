4441da7078d19f284ff350afee0531ba9e692083
DanielChvat <dchvat@gmail.com>
2024-03-08 10:09:13 -0800

    Base Project

---
+++ Connect4Project/main.cpp
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cppFiles/main.cc to edit this template
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: Daniel
+ *
+ * Created on March 8, 2024, 10:07 AM
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    cout << "Hello World" << endl;
+    
+    return 0;
+}
+
