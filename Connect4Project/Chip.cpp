11a6fcd39eced94eb746f60955b4bb7677aa8def
Michael <144060516+msalib1@users.noreply.github.com>
2024-03-19 00:52:13 -0700

    Update Chip.cpp

--- Connect4Project/Chip.cpp
+++ Connect4Project/Chip.cpp
@@ -12,12 +12,26 @@
 
 #include "Chip.h"
 
+/* 3/16/24 Noel
+ */
 Chip::Chip() {
+    this->color='P';
+ 
+}
+/* 3/16/24 Noel 
+ */
+Chip::Chip(char c){
+    this->color = c; 
 }
 
-Chip::Chip(const Chip& orig) {
-}
+
 
 Chip::~Chip() {
 }
 
+// Function that returns the chip color
+/* 3/16/24 Noel 
+ */
+char::Chip::getColor() const {
+    return this->color;
+}
