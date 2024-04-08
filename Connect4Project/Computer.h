2623b566d13e38d980e0f3b883b74e8ffdc6b6c7
Daniel Chvat <109172703+DanielChvat@users.noreply.github.com>
2024-04-08 13:21:39 -0700

    Update Computer.h

--- Connect4Project/Computer.h
+++ Connect4Project/Computer.h
@@ -1,26 +1,28 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/cppFiles/class.h to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cppFiles/file.h to edit this template
  */
 
 /* 
  * File:   Computer.h
- * Author: Daniel
+ * Author: michaelsalib
  *
- * Created on March 8, 2024, 11:29 AM
+ * Created on March 17, 2024
  */
+
+//User Libraries
+#include "Chip.h"
 
 #ifndef COMPUTER_H
 #define COMPUTER_H
-
-class Computer {
-public:
-    Computer();
-    Computer(const Computer& orig);
-    virtual ~Computer();
-private:
-
+class Computer{
+    private:
+        Chip chip;
+    public:
+        Computer();
+        int rTurn(int);
+        Chip getChip();
 };
 
-#endif /* COMPUTER_H */
 
+#endif /* COMPUTER_H */
